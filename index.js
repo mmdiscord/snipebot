2c35b6931fc7937c1d9323218ad7160bf45dcb73
mmdiscord <fortnitesverige.discord@gmail.com>
2018-09-27 22:32:35 +0200

    removed tooken

--- index.js
+++ index.js
@@ -100,4 +100,4 @@ bot.on('message', async (message) => {
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(commandfile) commandfile.run(bot,message,args);
 })
-bot.login("process.env.token");
+bot.login(process.env.token);
